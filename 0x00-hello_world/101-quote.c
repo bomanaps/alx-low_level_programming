142c220a5db3ed573c36e3078fd22da54089a6f6
bomanaps <mercy.boma35@yahoo.com>
2022-06-02 14:39:22 -0700

    My last task commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,17 +2,13 @@
 #include <unistd.h>
 
 /**
- * main - use to return the integer value 1
- *
- * Description: use to print to standard output in the main function
- *
- * Return: return the value 1
- *
+ * main - prints exactly "and that piece of art is useful"
+ * - Dora Korpar, 2015-10-19",
+ * followed by a new line, to the standard error.
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-write(2, "
-and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
-return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
