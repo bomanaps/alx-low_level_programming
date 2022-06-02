d64b38a49952ec0bb376643818b113ef14604182
bomanaps <mercy.boma35@yahoo.com>
2022-06-02 09:02:19 -0700

    My c task commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+
 /**
- * main - print the strings in the puts function
- * Description: prints "programming is like building a multilingual puzzel
- * Return: 0
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-puts("\"programming is like building a multilingual puzzle");
-return (0);
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
 }
