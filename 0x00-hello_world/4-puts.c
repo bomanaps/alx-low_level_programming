ac31be8b151c213c8f9bcf356b9f531281494de4
bomanaps <mercy.boma35@yahoo.com>
2022-06-02 08:50:43 -0700

    My beti task commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,11 @@
-#includ
+#include <stdio.h>
+/**
+ * main - print the strings in the puts function
+ * Description: prints "programming is like building a multilingual puzzel
+ * Return: 0
+ */
+int main(void)
+{
+puts("\"programming is like building a multilingual puzzle");
+return (0);
+}
