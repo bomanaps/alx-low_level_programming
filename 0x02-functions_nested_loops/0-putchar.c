2112aa643942593e81637a0697a6b3c1618eaab7
bomanaps <mercy.boma35@yahoo.com>
2022-06-07 14:10:27 -0700

    My first putchar task commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+Task 0
+
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char p[] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+	_putchar(p[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
