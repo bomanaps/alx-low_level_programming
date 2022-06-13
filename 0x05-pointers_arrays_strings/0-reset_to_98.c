68b870d12d4e38c68791377ee561b7975be9fc5f
bomanaps <mercy.boma35@yahoo.com>
2022-06-13 12:41:01 -0700

    My 1 string task commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function that takes a pointer to an int as a parameter
+ * and updates the value it points to to 98
+ *
+ * @n: int pointer variable
+ *
+ * Return - nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
